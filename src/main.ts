cc56ddfd3d783a113d384c20e670cdbf305fc974
minh.tranvan <minh.tranvan@hdwebsoft.io>
2022-07-01 15:58:58 +0700

    config asset css,js,images

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname,'..','public'));
   app.setBaseViewsDir(join(__dirname,'..','src/views'));
   app.setViewEngine('hbs');
   await app.listen(3000);
